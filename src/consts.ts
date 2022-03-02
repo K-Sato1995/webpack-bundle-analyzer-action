09e91aab959d2b28b513d219c656b6b3752afc69
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-02 12:41:24 +0900

    Change artifact name

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const ARTIFACT_NAME = 'K-Sato1995/webpack-bundle-analyzer-action'
+export const ARTIFACT_NAME = 'webpack-bundle-analyzer Report'
 export const GITHUB_BASE_PATH = 'https://github.com'
 export const ACTION_PATH = `${GITHUB_BASE_PATH}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`
 export const DOWNLOAD_COMMENT = 'Download the webpack-bundle-analyzer result'
