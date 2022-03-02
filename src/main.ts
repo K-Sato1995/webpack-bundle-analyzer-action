176468e91abcf19cb4ebe9cc352569a68451aa89
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-02 11:56:10 +0900

    create utils

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+/* eslint-disable sort-imports */
 import * as core from '@actions/core'
-import {ACTION_PATH} from './consts'
+import {DOWNLOAD_COMMENT} from './consts'
 import {analyzeBundle} from './analyze-bundle'
 import {createOrUpdateComment} from './create-or-update-comment'
 import github from '@actions/github'
 import {uploadReport} from './upload-artifact'
+import {constructMDTable, isJS, webpackStatsJson} from './utils'
 
 async function run(): Promise<void> {
   try {
@@ -22,8 +24,18 @@ async function run(): Promise<void> {
 
     if (prNumber) {
       core.info(`> Pull Request Number ${prNumber}`)
-      const message = `[Download the webpack-bundle-analyzer result](${ACTION_PATH})`
-      await createOrUpdateComment(octokit, prNumber, message)
+      const webpackStatsData = await webpackStatsJson('')
+      const jsFiles = webpackStatsData.assets.filter((asset: {name: string}) =>
+        isJS(asset.name)
+      )
+      const mdTable = constructMDTable(jsFiles)
+      const markdownComment = `
+      ## Overview
+      ${mdTable}
+      ## Check the detail
+      ${DOWNLOAD_COMMENT}
+      `
+      await createOrUpdateComment(octokit, prNumber, markdownComment)
     } else {
       core.info('> No pull request found.')
     }
---
+++ src/utils/index.ts
@@ -0,0 +1,39 @@
+// https://webpack.js.org/api/stats/#chunk-objects
+// https://github.com/webpack-contrib/webpack-bundle-analyzer/issues/61
+import type {WebpackAsset} from '../types'
+
+const webpackStatsJson = async (
+  pathToStatsJson: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<any> => {
+  const {default: jsonData} = await import(pathToStatsJson)
+  return JSON.parse(jsonData)
+}
+
+const isJS = (fileName: string): boolean => {
+  return fileName.split('.').pop() === 'js'
+}
+
+const formatBytes = (bytes: number, decimals = 2): string => {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+  let i = 0
+
+  for (i; bytes > 1024; i++) {
+    bytes /= 1024
+  }
+
+  return `${parseFloat(bytes.toFixed(decimals))} ${units[i]}`
+}
+
+const constructMDTable = (dataRows: WebpackAsset[]): string => {
+  const rowStrs = dataRows.map(({name, size}) => {
+    return `| \`${name}\` | ${formatBytes(size)} |`
+  })
+
+  return `| Name | Bundle Size(Parsed)  |
+  | ------ | ------ |
+  ${rowStrs.join('\n')}`
+}
+
+export {webpackStatsJson, isJS, formatBytes, constructMDTable}
